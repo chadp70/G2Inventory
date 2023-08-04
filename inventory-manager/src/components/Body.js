6b92c5ad2967544561ed413ce0cd4214bcc6327d
Chad Pearson <chad@chadpearson.com>
2023-08-04 10:35:06 -0400

    Added JSON file and modified body.js

--- inventory-manager/src/components/Body.js
+++ inventory-manager/src/components/Body.js
@@ -1,18 +1,19 @@
-import {Container, Row, Col} from "react-bootstrap";
-
+import { Container, Row, Col } from "react-bootstrap";
+import inventory from '../api/inventory.json';
+console.log(inventory);
 const Body = (props) => {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col xs lg="2">
-          &nbsp;
-        </Col>
-        <Col md="auto">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Enim nulla aliquet porttitor lacus. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Pretium fusce id velit ut. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat. Rutrum quisque non tellus orci ac auctor augue. Libero id faucibus nisl tincidunt eget nullam non nisi est. Justo laoreet sit amet cursus sit amet dictum sit amet. Vitae nunc sed velit dignissim sodales ut eu. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Magnis dis parturient montes nascetur ridiculus. Consequat nisl vel pretium lectus quam. Metus dictum at tempor commodo.</p>
-            <p>Arcu felis bibendum ut tristique et. Nec dui nunc mattis enim ut tellus elementum sagittis. Imperdiet dui accumsan sit amet nulla facilisi. Nisi vitae suscipit tellus mauris a diam. Mattis aliquam faucibus purus in massa. Elementum integer enim neque volutpat ac tincidunt vitae semper quis. Tortor posuere ac ut consequat semper viverra nam. Sollicitudin nibh sit amet commodo. Et netus et malesuada fames. Odio eu feugiat pretium nibh ipsum. Massa eget egestas purus viverra.</p></Col>
-        <Col xs lg="2">
-          &nbsp;
-        </Col>
+        <Col className="bg-secondary text-light">TAMCN</Col>
+        <Col className="bg-secondary text-light">AAC</Col>
+        <Col className="bg-secondary text-light">SUC</Col>
+        <Col className="bg-secondary text-light">Account Number</Col>
+        <Col className="bg-secondary text-light">Nomenclature</Col>
+        <Col className="bg-secondary text-light">NIIN</Col>
+        <Col className="bg-secondary text-light">Count of Serial #</Col>
+        <Col className="bg-secondary text-light">Sum of Quantity</Col>
+        <Col className="bg-secondary text-light">Sum of Unit Price</Col>
       </Row>
 
     </Container>
