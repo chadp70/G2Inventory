4011cabd02425cc79b05ae008bef7e3df08fc2c6
Chad Pearson <chad@chadpearson.com>
2023-08-09 07:08:28 -0400

    sync commit

--- inventory-manager/src/components/Body.js
+++ inventory-manager/src/components/Body.js
@@ -1,20 +1,12 @@
-import { Container, Row, Col } from "react-bootstrap";
+import {  Container, Row, Col } from "react-bootstrap";
 import Inventory from "./Inventory";
 const Body = (props) => {
   return (
     <>
-      <Row className="justify-content-md-center">
-        <Col className="bg-secondary text-light">TAMCN</Col>
-        <Col className="bg-secondary text-light">AAC</Col>
-        <Col className="bg-secondary text-light">SUC</Col>
-        <Col className="bg-secondary text-light">Account Number</Col>
-        <Col className="bg-secondary text-light">Nomenclature</Col>
-        <Col className="bg-secondary text-light">NIIN</Col>
-        <Col className="bg-secondary text-light">Count of Serial #</Col>
-        <Col className="bg-secondary text-light">Sum of Quantity</Col>
-        <Col className="bg-secondary text-light">Sum of Unit Price</Col>
-      </Row>
-      <Inventory />
+      <Container>
+
+        <Inventory />
+      </Container>
     </>
   );
 };
--- inventory-manager/src/components/Inventory.js
+++ inventory-manager/src/components/Inventory.js
@@ -1,32 +1,17 @@
-import { Container, Row, Col } from "react-bootstrap";
-import inventoryData from '../api/inventory.json';
+//import { Container, Row, Col } from "react-bootstrap";
+import inventoryData from '../api/NIIN.json';
 
 const Inventory = () => {
+    let arr = []
+    console.log(inventoryData)
+    Object.keys(inventoryData).forEach(function(key) {
+        arr.push(inventoryData[key]);
+      });
     return (
-        // inventoryData.forEach(function (data) {
-        <Row>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-            <Col>Foo</Col>
-        </Row>
-
-        //         <Col>{data.TAMCN}</Col>
-        //         <Col>{data.AAC}</Col>
-        //         <Col>{data.SUC}</Col>
-        //         <Col>{data.Account_Number}</Col>
-        //         <Col>{data.Nomenclature}</Col>
-        //         <Col>{data.NIIN}</Col>
-        //         <Col>{data.Count_of_Serial_Number}</Col>
-        //         <Col>{data.Sum_of_Quantity}</Col>
-        //         <Col>{data.Sum_of_Unit_Price}</Col>             
-        // })
+        <select onChange={(e) => alert(e.target.value)}>
+        {arr.map(data => (<option key={data.NIIN}>{data.Nomenclature}</option>))}
+        </select>
+        //         <Col>{data.TAMCN}</Col><Col>{data.AAC}</Col><Col>{data.SUC}</Col><Col>{data.Account_Number}</Col><Col>{data.Nomenclature}</Col><Col>{data.NIIN}</Col><Col>{data.Count_of_Serial_Number}</Col><Col>{data.Sum_of_Quantity}</Col><Col>{data.Sum_of_Unit_Price}</Col>             
 
     );
 
