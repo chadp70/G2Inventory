d9b85ae1f0969515c19c01e5be7062fe70b05340
Chad Pearson <chad@chadpearson.com>
2023-08-23 13:42:59 -0400

    reworked layout of form

--- inventory-manager/src/components/NewItemForm.js
+++ inventory-manager/src/components/NewItemForm.js
@@ -15,83 +15,97 @@ function NewItemForm() {
 
     setValidated(true);
   };
-
+//Item Name TAMCN AAC Account_Number NIIN Count Sum of Quantity Sum of Unit Price
   return (
     <>
+    
     <Card bg='light' >
       <Card.Header as="h5">Add New Inventory Item </Card.Header>
       <Card.Body>
         
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+      <Form.Group as={Col} md="3" controlId="validationCustom01">
+          <Form.Label>Item Name</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="First name"
-            defaultValue="Mark"
+            placeholder="Item Name"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+        <Form.Group as={Col} md="3" controlId="validationCustom02">
+          <Form.Label>TAMCN</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
+            placeholder="TAMCN"
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
+        <Form.Group as={Col} md="3" controlId="validationCustom03">
+          <Form.Label>AAC</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="ACC"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
+          <Form.Label>Account Number</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter Account Number"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
+
+
       </Row>
+      <Row className="mb-3">
+      <Form.Group as={Col} md="3" controlId="validationCustom05">
+          <Form.Label>NIIN</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="NIIN"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom06">
+          <Form.Label>Quantity</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Quantity"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom07">
+          <Form.Label>Unit Price</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Unit Price"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>  
+        <Form.Group as={Col} md="3" controlId="validationCustom08">
+          <Form.Label>Barcode Value</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Barcode Value"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>      
+        </Row>
       <Form.Group className="mb-3">
         <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
+          label="Mark item as currently unavailable"
         />
       </Form.Group>
-      <Button type="submit">Submit form</Button>
+      <Button type="submit">Add Item to Inventory</Button>
     </Form>
       </Card.Body>
     </Card>    
