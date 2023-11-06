edf225ad0c54cb0e862e5b97ddee88efb6a460c0
Chad Pearson <chad@chadpearson.com>
2023-11-06 13:20:31 -0500

    added cors

--- inventory-manager/backend/index.js
+++ inventory-manager/backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
-
 const app = express();
+const cors = require('cors')
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET","POST"]
+}))
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
