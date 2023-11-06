9e7359445f5b81df3752b84689447514917859fa
Chad Pearson <chad@chadpearson.com>
2023-11-06 09:20:51 -0500

    fixed issue with connection

--- inventory-manager/backend/index.js
+++ inventory-manager/backend/index.js
@@ -1,22 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
+const express = require('express');
+const bodyParser = require('body-parser');
+const routesHandler = require('./routes/handler.js');
 
-const app = express()
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/', routesHandler);
 
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({ extended: false }))
-
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
-app.use('/', router)
-
-const port = 4000
-const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+const PORT = 4000; // backend routing port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
