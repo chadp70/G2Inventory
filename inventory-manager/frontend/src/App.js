9fc83ac5ee83c08f5c04b8af0b7f64b99d7e78f7
Chad Pearson <christopher.c.pearson.ctr@socom.mil>
2023-11-13 14:28:19 -0500

    connected backend to frontend

--- inventory-manager/frontend/src/App.js
+++ inventory-manager/frontend/src/App.js
@@ -1,26 +1,48 @@
-import {useState, useEffect} from "react";
+
+import { Row, Col } from 'react-bootstrap';
+import { useState, useEffect, useRef } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   const [repos, setRepos] = useState([])
-  
-  const fetchData = async ()=>{
-  
-  let res = await fetch('http://127.0.0.1:4000/items')
-  let data = await res.json()
-  setRepos(data)
-  
+
+  const fetchData = async () => {
+
+    let res = await fetch('http://127.0.0.1:4000/items')
+    let data = await res.json()
+    setRepos(data)
+
   }
-  
-  
+
+
   useEffect(() => {
-      // fetch call used to be here
-      fetchData()
+    // fetch call used to be here
+    fetchData()
   }, [])
 
 
   return (
-      <div>
-
-      </div>
+    <>
+      <Row>&nbsp;</Row>
+      <Row>
+        <Col>
+          Select Inventory Item: <select><option>SELECT AN ITEM</option>
+            {repos.map(data => (<option key={data.ID} value={data.Nomenclature}>{data.Nomenclature}</option>))}
+          </select>
+        </Col>
+      </Row>
+      <Row>&nbsp;</Row>
+      <Row className="bg-secondary text-white">
+        <Col xs={3}>Nomenclature</Col>
+        <Col>TAMCN</Col>
+        <Col>AAC</Col>
+        <Col>Account_Number</Col>
+        <Col>NIIN</Col>
+        <Col>Count of Serial</Col>
+        <Col>Sum of Quantity</Col>
+        <Col>Sum of Unit Price</Col>
+      </Row>
+    </>
   )
 }
 
