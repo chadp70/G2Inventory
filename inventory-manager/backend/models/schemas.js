a245da0c448c51012117b1fe37055fb524f447cc
Chad Pearson <christopher.c.pearson.ctr@socom.mil>
2023-11-22 13:23:59 -0500

    scan works now

--- inventory-manager/backend/models/schemas.js
+++ inventory-manager/backend/models/schemas.js
@@ -2,19 +2,20 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const itemSchema = new Schema({
-    TAMCN: {type:String},
-    AAC: {type:String},
-    SUC: {type:String},
-    Account_Number: {type:String},
-    Nomenclature: {type:String},
-    NIIN: {type:String},
-    Count_of_Serial_Number: {type:String},
-    Sum_of_Quantity: {type:String},
-    Sum_of_Unit_Price: {type:String},
-    ScannerID: {type:String}
+    TAMCN: { type: String },
+    AAC: { type: String },
+    SUC: { type: String },
+    Account_Number: { type: String },
+    Nomenclature: { type: String },
+    NIIN: { type: String },
+    Count_of_Serial_Number: { type: String },
+    Sum_of_Quantity: { type: String },
+    Sum_of_Unit_Price: { type: String },
+    CheckedOut: { type: Boolean },
+    ScannerID: { type: String }
 })
 
 const Items = mongoose.model('Items', itemSchema, 'items')
-const mySchemas = {'Items':Items}
+const mySchemas = { 'Items': Items }
 
 module.exports = mySchemas
--- inventory-manager/frontend/src/components/ScanItem.js
+++ inventory-manager/frontend/src/components/ScanItem.js
@@ -1,59 +1,97 @@
-import {Row} from 'react-bootstrap';
-import {useState, useEffect, useRef} from "react";
+import { Row, Card, Col } from 'react-bootstrap';
+import { useState, useEffect, useRef } from "react";
 import "../css/ScanItem.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ItemResults from './ItemResults';
 import NewItemForm from './NewItemForm';
-const ScanItem = ({change}) => {
-    const scannerValue = useRef(); //Scanner value object for reference
-    scannerValue.current = "";
+const ScanItem = ({ change }) => {
+  const [scan, setScan] = useState('')
+  const [selected, setSelected] = useState('')
+  const scannerValue = useRef(''); //Scanner value object for reference
+  scannerValue.current = "";
 
-    const [scan,setScan] = useState();
-    const [newItem,setNewItem] = useState(false);
-    function handleChange(newValue) {
-      setNewItem(newValue);
-    }
-    
-    
-    useEffect(() => {
+  const [newItem, setNewItem] = useState(false);
+  function handleChange(newValue) {
+    setNewItem(newValue);
+  }
 
-      const handleKeydown = (e) => {
-        //Scanner must be set as keyboard emulator 
-        //On scan, last event value passed is "Enter" key press
-        if(e.key !== "Shift"){        //Ignore a "shift" key press as the correctly capitalized letter is sent anyway
-          if(e.key !== "Enter"){      
-            //If "Enter" hasn't been reached (last key press sent by scanner), add the charactor to the scannerValue object's "current" attribute
-            scannerValue.current += e.key;
-          
-          }else{
-            //Got to enter set the "scan" state
-            setScan(scannerValue.current);
-           //reset the scannerValue object's "current" attribute to an empty string
-            scannerValue.current = "";
-            e.target.value = "";
-          }
-          
+
+  const getItem = async (val) => {
+
+    let res = await fetch('http://127.0.0.1:4000/items/' + val)
+    let data = await res.json()
+    console.log(data);
+    setSelected(data[0]);
+  }
+
+
+  useEffect(() => {
+
+    const handleKeydown = (e) => {
+      //Scanner must be set as keyboard emulator 
+      //On scan, last event value passed is "Enter" key press
+
+      if (e.key !== "Shift") {        //Ignore a "shift" key press as the correctly capitalized letter is sent anyway
+        if (e.key !== "Enter") {
+          //If "Enter" hasn't been reached (last key press sent by scanner), add the charactor to the scannerValue object's "current" attribute
+          scannerValue.current += e.key;
+
+        } else {
+          //Got to enter set the "scan" state
+          setScan(scannerValue.current);
+          console.log(scannerValue.current)
+          getItem(scannerValue.current)
+          //reset the scannerValue object's "current" attribute to an empty string
+          scannerValue.current = "";
+          e.target.value = "";
         }
-        
-      };
-  
-      window.addEventListener("keydown", handleKeydown);
-      return () => {
-        window.removeEventListener("keydown", handleKeydown);
-      };
-    }, []);
 
-  
-    
+      }
+
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+
+
   return (
-    
-    <>   
-      <Row><h5>Scan Inventory Item or enter item barcode here and press the {"<Enter>"} key: <input id='scannedItem' type="text" 
-       /></h5></Row>
 
-        <ItemResults value={scan} change={handleChange}/>
-        <p>&nbsp;</p>
-        <NewItemForm display={newItem}/>
+    <>
+      <Row>
+        {selected != '' && <Card bg='light'>
+          <Card.Header as="h5">Results of Inventory Scan:</Card.Header>
+          <Card.Body>
+            <Row>
+              <Col xs={2}><strong>Item Name</strong></Col>
+              <Col><strong>TAMCN</strong></Col>
+              <Col><strong>AAC</strong></Col>
+              <Col><strong>Account_Number</strong></Col>
+              <Col><strong>NIIN</strong></Col>
+              <Col><strong>Count</strong></Col>
+              <Col xs={2}><strong>Sum of Quantity</strong></Col>
+              <Col xs={2}><strong>Sum of Unit Price</strong></Col>
+            </Row>
+
+
+            <Row key={selected._id} className="show-grid">
+              <Col xs={2}>{selected.Nomenclature}</Col>
+              <Col>{selected.TAMCN}</Col>
+              <Col>{selected.AAC}</Col>
+              <Col>{selected.Account_Number}</Col>
+              <Col>{selected.NIIN}</Col>
+              <Col>{selected.Count_of_Serial_Number}</Col>
+              <Col xs={2}>{selected.Sum_of_Quantity}</Col>
+              <Col xs={2}>{selected.Sum_of_Unit_Price}</Col>
+            </Row>
+          </Card.Body>
+        </Card>}
+      </Row>
+
+
+      <p>&nbsp;</p>
     </>
   );
 };
