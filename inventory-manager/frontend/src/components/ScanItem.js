76b2c08f4ccf2c98e5a94053f0b7e7cfbce653e5
Chad Pearson <christopher.c.pearson.ctr@socom.mil>
2023-12-11 15:00:43 -0500

    Added comments to ScanItem.js

--- inventory-manager/frontend/src/components/ScanItem.js
+++ inventory-manager/frontend/src/components/ScanItem.js
@@ -18,52 +18,61 @@ const ScanItem = ({ change }) => {
   const [SUC, setSUC] = useState('')
   const [Available, setAvailable] = useState(true)
   const [CheckedOutTo, setCheckedOutTo] = useState('')
+
+
   const scannerValue = useRef(''); //Scanner value object for reference
 
+
+  //getItem function received the scanned item's code and searches the database for a match
   const getItem = async (val) => {
-    let res = await fetch('http://127.0.0.1:4000/items/' + val)
-    let data = await res.json()
+    let res = await fetch('http://127.0.0.1:4000/items/' + val) // Results of the fetch call are assigned to res variable
+
+    let data = await res.json() //return the results in JSON format and assign them to the data variable 
     let newText
-    console.log(data);
-    if(data.length>0){
-      setSelected(data[0]);
-      setShowSelected(true);
-      setHeaderText('')
+
+    if (data.length > 0) { //If data is returned, this is executed
+      setSelected(data[0]); //Set the state of the "selected" object to the value returned from the fetch (JSON dataset's first object (there's always only one but still have to account for it))
+      setShowSelected(true); //set "showSelected" state to true which will cause the display form to render
+      setHeaderText('') //Clear the header text 
+
       newText = `Do you wish to check this item out?`
-      setHeaderText(newText)
-    }else{
-      setScannerID(val)
+      setHeaderText(newText) //Set the header text
+
+    } else {            //If no data is returned, this is executed
+      setScannerID(val) //Assign the scanned barcode to the "scannerID" state
+
       newText = `This item with the bar code '${val}' is not in the current Inventory database. Would you like to add it now?`
+
       setHeaderText(newText)
-      setNewChoice(true)
-      setShowSelected(false)
-      
+      setNewChoice(true) //Set "newChoice" state to true which renders the empty form for data entry of new item
+      setShowSelected(false) // hide the "showSelected" form
+
     }
-    
+
   }
 
 
   const addItemToInventory = async (e) => {
-      e.preventDefault();
-      let result = await fetch(
+    e.preventDefault();
+    let result = await fetch(
       'http://localhost:4000/', {
-          method: "post",
-          body: JSON.stringify({ TAMCN, AAC, SUC, Account_Number, Nomenclature, NIIN, ScannerID, Available, CheckedOutTo }),
-          headers: {
-              'Content-Type': 'application/json'
-          }
-      })
+      method: "post",
+      body: JSON.stringify({ TAMCN, AAC, SUC, Account_Number, Nomenclature, NIIN, ScannerID, Available, CheckedOutTo }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
 
     result = await result.json();
     console.warn(result);
     if (result) {
-        alert("Data saved succesfully");
-        scannerValue.current = ''
-        setNomenclature('');
-        setTAMCN('');
-        setAAC('');
-        setAAC('');
-        setAccount_Number('');
+      alert("Data saved succesfully");
+      scannerValue.current = ''
+      setNomenclature('');
+      setTAMCN('');
+      setAAC('');
+      setAAC('');
+      setAccount_Number('');
     }
   }
 
@@ -73,37 +82,37 @@ const ScanItem = ({ change }) => {
     let ScannerID = selected.ScannerID
     let Available = selected.Available
     let CheckedOutTo = selected.CheckedOutTo
-    try{
+    try {
       let result = await fetch(
         'http://localhost:4000/', {
-            method: "patch",
-            body: JSON.stringify({ScannerID, Available, CheckedOutTo }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    
+        method: "patch",
+        body: JSON.stringify({ ScannerID, Available, CheckedOutTo }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
       result = await result.json();
       console.warn(result);
       if (result) {
-          alert("Data Updated succesfully");
-          scannerValue.current = ''
-          setAvailable(false)
-          setCheckedOutTo(result.CheckedOutTo)
+        alert("Data Updated succesfully");
+        scannerValue.current = ''
+        setAvailable(false)
+        setCheckedOutTo(result.CheckedOutTo)
       }
-    }catch(e){
+    } catch (e) {
       alert(e.message);
     }
 
-}
+  }
 
-  const handleYes = ()=>{
+  const handleYes = () => {
     setAddItem(true)
     setNewChoice(false)
     setHeaderText('')
   }
 
-  const handleCancel = ()=>{
+  const handleCancel = () => {
     setAddItem(false)
     setNewChoice(false)
     setHeaderText('Scan an item to begin:')
@@ -143,142 +152,142 @@ const ScanItem = ({ change }) => {
 
   return (
 
-      
+
     <>
       <Row>
         <h5 className='clsHeader'>{headerText}</h5>
       </Row>
       <Row>
-        {showSelected && 
-        <>
-        <Row>
-          <Col xs={2}>&nbsp;</Col>
-          <Col><h1>Check out Inventory</h1></Col>
-        </Row>
-        <Row>
-          <Col xs={2}>&nbsp;</Col>
-          <Col>
-          <Table striped bordered>
-            <thead>
-              <tr>
-                <th>Barcode</th>
-                <th>Name</th>
-                <th>TAMCN</th>
-                <th>AAC</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{selected.ScannerID}</td>
-                <td>{selected.Nomenclature}</td>
-                <td>{selected.TAMCN}</td>
-                <td>{selected.AAC}</td>
-              </tr>
-            </tbody>
-            <thead>
-              <tr>
-                <th>Account Number</th>
-                <th>NIIN</th>
-                <td colSpan={2}>&nbsp;</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{selected.Account_Number}</td>
-                <td>{selected.NIIN}</td>
-                <td colSpan={2}>&nbsp;</td>
-              </tr>
-              <tr>
-                <td colSpan={4}>
-                <Button variant="primary"  onClick={handleCheckItemInOut}>
-                {Available ? "Check out" : "Check in"}
-              </Button>
-              &nbsp;              
-              <Button variant="danger" onClick={handleCancel}>
-                Cancel
-              </Button>
-              </td>
-              </tr>
-            </tbody>
+        {showSelected &&
+          <>
+            <Row>
+              <Col xs={2}>&nbsp;</Col>
+              <Col><h1>Check out Inventory</h1></Col>
+            </Row>
+            <Row>
+              <Col xs={2}>&nbsp;</Col>
+              <Col>
+                <Table striped bordered>
+                  <thead>
+                    <tr>
+                      <th>Barcode</th>
+                      <th>Name</th>
+                      <th>TAMCN</th>
+                      <th>AAC</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{selected.ScannerID}</td>
+                      <td>{selected.Nomenclature}</td>
+                      <td>{selected.TAMCN}</td>
+                      <td>{selected.AAC}</td>
+                    </tr>
+                  </tbody>
+                  <thead>
+                    <tr>
+                      <th>Account Number</th>
+                      <th>NIIN</th>
+                      <td colSpan={2}>&nbsp;</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{selected.Account_Number}</td>
+                      <td>{selected.NIIN}</td>
+                      <td colSpan={2}>&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td colSpan={4}>
+                        <Button variant="primary" onClick={handleCheckItemInOut}>
+                          {Available ? "Check out" : "Check in"}
+                        </Button>
+                        &nbsp;
+                        <Button variant="danger" onClick={handleCancel}>
+                          Cancel
+                        </Button>
+                      </td>
+                    </tr>
+                  </tbody>
 
-          </Table>
-          </Col>
-          <Col xs={2}>&nbsp;</Col>
-        </Row>
+                </Table>
+              </Col>
+              <Col xs={2}>&nbsp;</Col>
+            </Row>
 
-        </>
+          </>
         }
 
         {addItem && <>
-        <Row>
-          <Col xs={2}>&nbsp;</Col>
-          <Col><h1>Add an Item to Inventory</h1></Col>
-        </Row>
-        <Row>
-          <Col xs={2}>&nbsp;</Col>
-          <Col>
-          <Table striped bordered>
-            <thead>
-              <tr>
-                <th>Barcode</th>
-                <th>Name</th>
-                <th>TAMCN</th>
-                <th>AAC</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><Form.Control type="text" id='barCode' value={ScannerID} disabled/></td>
-                <td><Form.Control type="text" id='itemName' placeholder="Item Name" onInput={e => setNomenclature(e.target.value)}/></td>
-                <td><Form.Control type="text" id='TAMCN' placeholder="TAMCN" onInput={e => setTAMCN(e.target.value)}/></td>
-                <td><Form.Control type="text" id='AAC' placeholder="AAC" onInput={e => setAAC(e.target.value)}/></td>
-              </tr>
-            </tbody>
-            <thead>
-              <tr>
-                <th>Account Number</th>
-                <th>NIIN</th>
-                <th colSpan={2}>SUC</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><Form.Control type="text" id='Account' placeholder="Account Number" onInput={e => setAccount_Number(e.target.value)}/></td>
-                <td><Form.Control type="text" id='NIIN' placeholder="NIIN" onInput={e => setNIIN(e.target.value)}/></td>
-                <td colSpan={2}><Form.Control type="text" id='SUC' placeholder="ex - YBOATS" onInput={e => setSUC(e.target.value)}/></td>
-              </tr>
-              <tr>
-                <td colSpan={4}>
-                <Button variant="primary" onClick={addItemToInventory}>
-                Add Item to Inventory
-              </Button>
-              &nbsp;              
-              <Button variant="danger" onClick={handleCancel}>
-                Cancel
-              </Button>
-              </td>
-              </tr>
-            </tbody>
+          <Row>
+            <Col xs={2}>&nbsp;</Col>
+            <Col><h1>Add an Item to Inventory</h1></Col>
+          </Row>
+          <Row>
+            <Col xs={2}>&nbsp;</Col>
+            <Col>
+              <Table striped bordered>
+                <thead>
+                  <tr>
+                    <th>Barcode</th>
+                    <th>Name</th>
+                    <th>TAMCN</th>
+                    <th>AAC</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><Form.Control type="text" id='barCode' value={ScannerID} disabled /></td>
+                    <td><Form.Control type="text" id='itemName' placeholder="Item Name" onInput={e => setNomenclature(e.target.value)} /></td>
+                    <td><Form.Control type="text" id='TAMCN' placeholder="TAMCN" onInput={e => setTAMCN(e.target.value)} /></td>
+                    <td><Form.Control type="text" id='AAC' placeholder="AAC" onInput={e => setAAC(e.target.value)} /></td>
+                  </tr>
+                </tbody>
+                <thead>
+                  <tr>
+                    <th>Account Number</th>
+                    <th>NIIN</th>
+                    <th colSpan={2}>SUC</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><Form.Control type="text" id='Account' placeholder="Account Number" onInput={e => setAccount_Number(e.target.value)} /></td>
+                    <td><Form.Control type="text" id='NIIN' placeholder="NIIN" onInput={e => setNIIN(e.target.value)} /></td>
+                    <td colSpan={2}><Form.Control type="text" id='SUC' placeholder="ex - YBOATS" onInput={e => setSUC(e.target.value)} /></td>
+                  </tr>
+                  <tr>
+                    <td colSpan={4}>
+                      <Button variant="primary" onClick={addItemToInventory}>
+                        Add Item to Inventory
+                      </Button>
+                      &nbsp;
+                      <Button variant="danger" onClick={handleCancel}>
+                        Cancel
+                      </Button>
+                    </td>
+                  </tr>
+                </tbody>
 
-          </Table>
-          </Col>
-          <Col xs={2}>&nbsp;</Col>
-        </Row>
+              </Table>
+            </Col>
+            <Col xs={2}>&nbsp;</Col>
+          </Row>
 
         </>}
 
-        {newChoice && 
-        <>
-        <Card bg='light'>
-        
-          <Card.Body>
-          <Button variant="primary" onClick={handleYes}>
-                Yes</Button>&nbsp;
-          <Button variant="danger" onClick={handleCancel}>
-                Cancel</Button>
-          </Card.Body>
-        </Card>
-        </>}
+        {newChoice &&
+          <>
+            <Card bg='light'>
+
+              <Card.Body>
+                <Button variant="primary" onClick={handleYes}>
+                  Yes</Button>&nbsp;
+                <Button variant="danger" onClick={handleCancel}>
+                  Cancel</Button>
+              </Card.Body>
+            </Card>
+          </>}
 
       </Row>
 
